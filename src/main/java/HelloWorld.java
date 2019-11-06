3373027b49bf4d2c5a72bc3c5fa51c673175c9de
BintRassoul <fatimadiakite0@gmail.com>
2019-11-06 16:08:13 +0100

    second commit

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -2,7 +2,10 @@ package main.java;
 
 public class HelloWorld {
 
-	public String getMessage() {return "Hello World";}
+	public String getMessage(String name) {
+		if(name!=null)
+			return "Hello "+name;
+		return "Hello World";}
 	public int sum(int a, int b) {return a+b;}
 	
 	
