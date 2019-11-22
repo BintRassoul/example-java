919cc2078cffdb4c2a313dee1389b0169912867e
BintRassoul <fatimadiakite0@gmail.com>
2019-11-22 14:15:43 +0100

    example_java last update 2

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,12 +1,7 @@
 package main.java;
 
 public class HelloWorld {
-
-	public String getMessage(String name) {
-		if(name!=null)
-			return "Hello "+name;
-		return "Hello World";}
-	public int sum(int a, int b) {return a+b;}
-	
+	public static int sum(int a, int b) {return a+b;}
+	public static  int mult(int a, int b) {return a*b;}
 	
 }
